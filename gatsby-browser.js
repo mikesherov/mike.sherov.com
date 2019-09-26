b2c1cf767ae90dda576324fba3567816db3a4f60
Mike Sherov <mike.sherov@gmail.com>
2019-09-26 14:05:33 -0400

    switch to solarized-light

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
-require("prismjs/themes/prism-okaidia.css")
+require("prismjs/themes/prism-solarizedlight.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("./src/theme/prism.css")
